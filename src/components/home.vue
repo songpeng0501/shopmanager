a860c8823a9a1cce1eb42624c8aba8a8ece476a0
ynn <1219221972@qq.com>
2019-02-16 17:17:35 +0800

    登陆页面 -布局

---
+++ src/components/home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    home---
+  </div>
+</template>
+
+<script>
+export default {
+  components:{},
+  props:{},
+  data(){
+    return {
+    }
+  },
+  watch:{},
+  computed:{},
+  methods:{},
+  created(){},
+  mounted(){}
+}
+</script>
+<style>
+</style>
